1ae946ae7ab0b97c6c48a33404d4af83cff1a091
francescoP02 <file.partipilo@gmail.com>
2022-10-06 12:56:55 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,96 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            using (PizzaContext context = new PizzaContext())
+            {
+                List<Pizza> pizzas = context.pizzasList.ToList<Pizza>();
+                return View("Index", pizzas);
+            }
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", formData);
+            }
+
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pizza = new Pizza();
+                pizza.Name = formData.Name;
+                pizza.Description = formData.Description;
+                pizza.Photo = formData.Photo;
+                pizza.Price = formData.Price;
+
+                context.pizzasList.Add(pizza);
+
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult Show(int id)
+        {
+
+            using (PizzaContext db = new PizzaContext())
+            {
+
+                try
+                {
+                    Pizza p = db.pizzasList.Where(x => x.Id == id).First<Pizza>();
+                    return View("Show", p);
+                }
+                catch
+                {
+                    return View("Error");
+                }
+
+            }
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+
+}
